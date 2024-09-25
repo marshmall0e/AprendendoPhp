394cd4a8ad0f14921d7200e3c94415d661f70a40
marshmall0e <155745312+marshmall0e@users.noreply.github.com>
2024-09-25 08:49:10 -0300

    Subindo exemplos de data

---
+++ Funções de Data/ExData01.php
@@ -0,0 +1,9 @@
+<?php 
+
+echo date("d/m/Y H:i:s", 1727263750);
+
+echo"<br>";
+
+echo time();
+
+?>
---
+++ Funções de Data/ExData02.php
@@ -0,0 +1,7 @@
+<?php 
+
+//$ts =  strtotime("2001-09-11");
+$ts =  strtotime("+1 month");
+echo date("1, d/m/y", $ts);
+
+?>
