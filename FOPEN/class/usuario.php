b3881b0ce6bb1f4f14b4b990b90f2548b3c675a4
marshmall0e <155745312+marshmall0e@users.noreply.github.com>
2024-10-03 08:02:52 -0300

    exemplos Fopen

---
+++ FOPEN/class/usuario.php
@@ -0,0 +1,168 @@
+<?php 
+
+class Usuario {
+
+    private $idusuarios;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    // Métodos de acesso (getters e setters)
+    public function getIdusuarios() {
+        return $this->idusuarios;
+    }
+    
+    public function setIdusuarios($value) {
+        $this->idusuarios = $value;
+    }
+
+    public function getDeslogin() {
+        return $this->deslogin;
+    }
+    
+    public function setDeslogin($value) {
+        $this->deslogin = $value;
+    }
+
+    public function getDessenha() {
+        return $this->dessenha;
+    }
+    
+    public function setDessenha($value) {
+        $this->dessenha = $value;
+    }
+
+    public function getDtcadastro() {
+        return $this->dtcadastro;
+    }
+    
+    public function setDtcadastro($value) {
+        $this->dtcadastro = $value;
+    }
+
+    // Carrega um usuário pelo ID
+    public function loadById($id) {
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID" => $id));
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        }
+    }
+
+    // Obtém a lista de usuários
+    public static function getList() {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+    }
+
+    // Busca usuários pelo login
+    public static function search($login) {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+            ':SEARCH' => "%" . $login . "%"
+        ));
+    }
+
+    // Faz // Faz login do usuário
+    public function login($login, $password) {
+    $sql = new Sql();
+    $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+        ":LOGIN" => $login,
+        ":PASSWORD" => $password
+    ));
+
+    // Adicionando var_dump para depuração
+    var_dump($results); // Veja o que está sendo retornado
+
+    if (is_array($results) && count($results) > 0) {
+        $this->setData($results[0]);
+    } else {
+        throw new Exception("Login e/ou senha inválidos.");
+        }
+    }
+
+
+    
+    // Define os dados do usuário
+    /*public function setData($data) {
+    if (is_array($data) && isset($data['idusuario'], $data['deslogin'], $data['dessenha'], $data['dtcadastro'])) {
+        $this->setIdusuarios($data['idusuario']);
+        $this->setDeslogin($data['deslogin']);
+        $this->setDessenha($data['dessenha']);
+        $this->setDtcadastro(new DateTime($data['dtcadastro']));
+    } else {
+        throw new Exception("Dados inválidos para setData: " . print_r($data, true));
+        }
+    }*/
+
+    public function setData($data) {
+        if (isset($data['deslogin']) && isset($data['dessenha'])) {
+            // O id e a data de cadastro não são necessários no momento da inserção
+            $this->setDeslogin($data['deslogin']);
+            $this->setDessenha($data['dessenha']);
+            // Para o cadastro, podemos definir dtcadastro aqui ou deixá-lo como NULL para o banco de dados tratar
+            $this->setDtcadastro(new DateTime()); // Se quiser definir agora
+        } else {
+            throw new Exception("Dados inválidos para setData: " . print_r($data, true));
+        }
+    }
+    
+
+
+    // Insere um novo usuário
+    public function insert() {
+        $sql = new Sql();
+        $results = $sql->select("CALL sp_usuarios_insert(:LOGIN, :PASSWORD)", array(
+            ':LOGIN' => $this->getDeslogin(),
+            ':PASSWORD' => $this->getDessenha()
+        ));
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        }
+    }
+
+    public function update($Login, $password) {
+        $this->setDeslogin($Login);
+        $this->setDessenha($password);
+
+        $sql = new Sql();
+        $sql->execQuery("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID", array(
+        ':LOGIN'=>$this->getDeslogin(),
+        ':PASSWORD'=> $this->getDessenha(),
+        ':ID'=>$this->getIdusuarios()
+        ));
+    }
+
+    public function delete(){
+        $sql = new Sql();
+        $sql->execQuery("DELETE FROM tb_usuarios WHERE idusuario = :ID", array(
+            ':ID'=>$this->getIdusuarios(),
+        ));
+
+        $this->setIdusuarios(0);
+        $this->setDeslogin("");
+        $this->setDessenha("");
+        $this->setDtcadastro(new DateTime());
+    }
+
+    public function __construct($login = "", $password = "")
+    {
+        $this->setDeslogin($login);
+        $this->setDessenha($password); // Use setDessenha em vez de setPassword
+    }
+    
+
+    // Representação do objeto como string
+    public function __toString() {
+        return json_encode(array(
+            "idusuario" => $this->getIdusuarios(),
+            "deslogin" => $this->getDeslogin(),
+            "dessenha" => $this->getDessenha(),
+            "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
+}
+
+?>
---
+++ FOPEN/index.php
@@ -0,0 +1,65 @@
+<?php 
+
+require_once 'class/config.php';
+
+/*$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios);*/
+
+//Carrega apenas um usuário
+//$root = new Usuario();
+//$root->loadById(22);
+//echo $root;
+
+//Carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega uma lista de usuarios buscando pelo login
+//$search =  Usuario::search("li");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e senha 
+//$usuario = new Usuario();
+//$usuario->login("marshmall0e", "onmyown123");
+//echo $usuario;
+
+
+/*Criando um novo usuario
+$aluno = new Usuario();
+
+
+// Usar o método setData para definir um array associativo
+$data = [
+    'deslogin' => 'aluno',
+    'dessenha' => '@lun0',
+];
+
+// O método setData foi modificado para aceitar um array diretamente
+$aluno->setData($data);
+
+// Inserindo o usuário no banco de dados
+$aluno->insert();
+
+// Mostrando os dados do usuário recém-inserido
+echo $aluno;*/
+
+/*//alterar usuario 
+$usuario = new Usuario();
+
+$usuario->loadById(27);
+
+$usuario->update("professor", "!@#$%¨&*()");
+
+echo $usuario;*/
+//deletando usuarios
+$usuario = new Usuario();
+
+$usuario->loadById(27);
+
+$usuario->delete();
+
+echo $usuario;
+?>
